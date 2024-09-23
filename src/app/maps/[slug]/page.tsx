8bf512c7c302e5f5302454f12bea12f490cf1b13
Eric Fung <eric.fung@iheartjane.com>
2024-09-22 20:07:08 -0400

    formatted list of places

--- src/app/maps/[slug]/page.tsx
+++ src/app/maps/[slug]/page.tsx
@@ -1,13 +1,27 @@
+import { PlaceSummary } from "@/components/PlaceSummary";
+import { placeDetail } from "@/dataAccess/googlePlaces/client";
+
 interface MapDetailProps {
   params: {
     slug: string
   }
 }
 
-export default function MapDetail({ params: { slug } }: MapDetailProps) {
+export default async function MapDetail({ params: { slug } }: MapDetailProps) {
+  const places = await Promise.all([
+    placeDetail('ChIJCar0f49ZwokR6ozLV-dHNTE'),
+    placeDetail('ChIJQ-hJIuBbwokRRUdquw_5w5U'),
+  ])
   return (
     <main className="content">
       <h1 className="heading">Map {slug}</h1>
+      <div>
+      {
+        places.map((place) => (
+          <PlaceSummary key={place.id} placeDetail={place} />
+        ))
+      }
+      </div>
     </main>
   );
 }
